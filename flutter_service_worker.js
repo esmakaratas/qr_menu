2b54ca7433d99d0a795a6e3c4b4ffcff43c6fbeb
esmakaratas <karatasesma925@gmail.com>
2025-05-28 14:19:12 +0300

    Son güncelleme

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3a35b5651fcbff31805d9ca0219d735a
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "8a27c02312a80bc66eb27df502a2f3d9",
+"flutter_bootstrap.js": "93e070fa8dbef6b272edd16d2698c356",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
